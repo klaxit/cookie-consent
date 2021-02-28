d4ecb18954d2fd1e3bef73ed82e166fc018273ad
Cyrille Courtiere <cyrille.courtiere@gmail.com>
2021-03-01 00:28:10 +0100

    Cookie object to serialize/deserialize consent state

---
+++ src/cookie.js
@@ -0,0 +1,45 @@
+export default class Cookie {
+  constructor(cookieOptions) {
+    this.cookieOptions = cookieOptions
+    this.load()
+  }
+
+  dump() {
+    const serialized = JSON.stringify({
+      status: this.status,
+      acceptedCategories: this.acceptedCategories
+    })
+
+    var cookieStr = this.cookieOptions.cookieName + "=" + serialized
+
+    const expDate = new Date()
+    const expDays = this.cookieOptions.expiryDays
+    const expHours = (typeof expDays !== "number"  ? 365 : expDays ) * 24
+    expDate.setHours(expDate.getHours() + expHours)
+    cookieStr += "; expires=" + expDate.toUTCString()
+
+    cookieStr += "; path=/"
+    cookieStr += (this.cookieOptions.secure ? "; secure" : "")
+    cookieStr += (this.cookieOptions.domain ? "; domain=" + this.cookieOptions.domain : "")
+
+    document.cookie = cookieStr
+  }
+
+  load() {
+    const existingConsent = this._getCookie(this.cookieOptions.name)
+    if(existingConsent){
+      parsed = JSON.parse(existingConsent)
+      this.status = parsed.status
+      this.acceptedCategories = parsed.acceptedCategories
+    } else {
+      this.status =  null
+      this.acceptedCategories = []
+    }
+  }
+
+  _getCookie(cookieName) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${cookieName}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+}
